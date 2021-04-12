b1b7537bcb9bb21ffa1ecab995deff5ffa2b82f4
merrell-b <54639901+merrell-b@users.noreply.github.com>
2021-04-11 18:39:53 -0700

    Update run-H2.sh

--- run-H2.sh
+++ run-H2.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=05:00:00
 #SBATCH --output=array_job.out
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jbneaton@berkeley.edu
+#SBATCH --mail-user=mbrzeczek@berkeley.edu
 
 module load quantumespresso/6.3
 
