908c56424e719b70d27725d31e0cd722d5049b8e
merrell-b <54639901+merrell-b@users.noreply.github.com>
2021-04-12 10:04:16 -0700

    Create run-InPbands.sh

---
+++ run-InPbands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=InPbands
+#SBATCH --account=ic_phys190
+#SBATCH --partition=savio2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=15
+#SBATCH --time=05:00:00
+#SBATCH --output=array_job.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=mbrzeczek@berkeley.edu
+
+module load quantumespresso/6.3
+
+pw.x < InPbands.in > InPbands.out
