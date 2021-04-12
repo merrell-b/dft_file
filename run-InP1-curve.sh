6a092ce2567cb221c5dba4fd15de670cdae2997c
merrell-b <54639901+merrell-b@users.noreply.github.com>
2021-04-11 19:59:08 -0700

    Update run-InP1-curve.sh

--- run-InP1-curve.sh
+++ run-InP1-curve.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=molecular_h2
+#SBATCH --job-name=InP_1
 #SBATCH --account=ic_phys190
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=15
-#SBATCH --time=05:00:00
+#SBATCH --time=00:10:00
 #SBATCH --output=array_job.out
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=jbneaton@berkeley.edu
+#SBATCH --mail-user=mbrzeczek@berkeley.edu
 
 module load quantumespresso/6.3
 
 for i in 0.03 0.05 0.06 0.07 0.08 0.09 0.10 0.11 0.13 0.15 0.17 0.25 0.30 0.35; do
 
-cat <<! > H2.$i.in
+cat <<! > InP.$i.in
  &control
     calculation = 'scf'
     verbosity='high'
     restart_mode='from_scratch',
-    prefix='H2',
+    prefix='InP',
     tstress = .true.
     tprnfor = .true.
-    pseudo_dir = '/global/home/users/jbneaton/H2_LDA',
-    outdir='/global/home/users/jbneaton/test_H2_LDA'
+    pseudo_dir = '.',
+    outdir='.'
  /
  &system
     ibrav=  1,
@@ -31,20 +31,52 @@ cat <<! > H2.$i.in
 	ntyp= 1,
     ecutwfc = 50
  /
- &electrons
-    mixing_beta = 0.7
-    conv_thr =  1.0d-8
- /
-ATOMIC_SPECIES
- H  1.00794  H.pz-rrkjus_psl.1.0.0.UPF
+&electrons
+   electron_maxstep = 100
+   conv_thr = 1e-08
+   mixing_mode = 'plain'
+   mixing_beta = 0.7
+   mixing_ndim = 8
+   diagonalization = 'david'
+   diago_david_ndim = 4
+   diago_full_acc = .true.
+/
+CELL_PARAMETERS angstrom
+   0.0 2.93435 2.93435
+   2.93435 0.0 2.93435
+   2.93435 2.93435 0.0
+ATOMIC_SPECIES 
+   In 114.818 'In1.upf'
+   P 30.973762 'P1.upf'
 ATOMIC_POSITIONS crystal
- H 0.00 0.00 0.00
- H $i 0.00 0.00
-K_POINTS automatic
-1 1 1 0 0 0
-EOF
+   P -$i -0.125 -0.125
+   In $i 0.125 0.125
+K_POINTS crystal
+   22
+   0.0 0.0 0.0 1.0
+   0.0 0.0 0.166666667 4.0
+   0.0 0.0 0.333333333 4.0
+   0.0 0.0 0.5 4.0
+   0.0 0.0 0.666666667 4.0
+   0.0 0.0 0.833333333 4.0
+   0.0 0.166666667 0.166666667 6.0
+   0.0 0.166666667 0.333333333 12.0
+   0.0 0.166666667 0.5 12.0
+   0.0 0.166666667 0.666666667 12.0
+   0.0 0.166666667 0.833333333 12.0
+   0.0 0.333333333 0.333333333 6.0
+   0.0 0.333333333 0.5 12.0
+   0.0 0.333333333 0.666666667 12.0
+   0.0 0.333333333 0.833333333 12.0
+   0.0 0.5 0.5 3.0
+   0.0 0.5 0.666666667 12.0
+   0.0 0.5 0.833333333 12.0
+   0.0 0.666666667 0.833333333 12.0
+   0.166666667 0.333333333 0.5 24.0
+   0.166666667 0.333333333 0.666666667 24.0
+   0.166666667 0.5 0.666666667 12.0
 !
 
-pw.x < H2.$i.in > H2.$i.out
+pw.x < InP1.$i.in > InP1.$i.out
 
 done
